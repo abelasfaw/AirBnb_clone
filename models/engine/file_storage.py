77ec8d52d4e868e353a3b34a6acc7732c41a9de5
abelasfaw <abeloman89@gmail.com>
2022-10-30 04:41:10 -0700

    fix file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,27 +12,37 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
+    def getValidClasses(self):
+        """return a dictionary of supported classes"""
+
+        from models.base_model import BaseModel
+        valid_classes = {"BaseModel": BaseModel}
+        return valid_classes
+
     def all(self):
         """returns __objects """
 
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """ adds new instance to __objects"""
 
-        key = obj.__class__.__name__ + "." + obj.id
-        self.__objects[key] = obj.to_dict()
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """saves dictionary to json file"""
 
-        with open(self.__file_path, 'w') as json_file:
-            json.dump(self.__objects, json_file)
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
+            d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
+            json.dump(d, f)
 
     def reload(self):
         """loads objects from json file"""
 
         if(path.exists(self.__file_path)):
             with open(self.__file_path) as json_file:
-                print("inside ", json_file)
-                self.__objects = json.load(json_file)
+                loaded_dict = json.load(json_file)
+                loaded_dict = {k: self.getValidClasses()[v["__class__"]]
+                               (**v) for k, v in loaded_dict.items()}
+                FileStorage.__objects = loaded_dict
