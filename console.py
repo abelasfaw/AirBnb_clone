f10137e738a045aafcc2a215f4ecc28363f9fe4e
abelasfaw <abeloman89@gmail.com>
2022-10-30 00:56:56 -0700

    fix formatting

--- console.py
+++ console.py
@@ -1,19 +1,42 @@
 #!/usr/bin/python3
+"""Module to be used as the entry point of the comand interpreter"""
+
+
 import cmd
+
+
 class HBNBCommand(cmd.Cmd):
+    """HBNBCommand class"""
+
     prompt = '(hbnb) '
+
     def emptyline(self):
+        """handles empty line"""
+
         pass
+
     def do_EOF(self, line):
+        """hanldes EOF"""
+
         return True
+
     def help_EOF(self):
+        """help for EOF"""
+
         print("CTRL + D to exit the program")
         print()
+
     def do_quit(self, line):
+        """handles quit command"""
+
         return True
+
     def help_quit(self):
+        """help for quit command"""
+
         print("Quit command to exit the program")
         print()
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
